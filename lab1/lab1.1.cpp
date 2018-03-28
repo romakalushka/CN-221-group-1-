04d79d750360cbe172d02fe6e5c5a395d7194d7f
romakalushka <rkalushka@gmail.com>
2018-03-28 14:02:40 +0300

    lab1(1-4)

---
+++ lab1/lab1.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+  int main()
+  {
+  
+int n, V;
+	const double fut=7.481;
+	cout<<("write amount of galons n");
+	cin>>n;
+	cout<<("V=",fut/n)<<endl;
+	system("pause");
+	return 0;
+	
+}
---
+++ lab1/main lab1(2).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+ int main(){
+ 
+cout<<" 1900 135\n 1991 7290\n 1993 1130\n 1994 16200\n ";
+system ("pause");
+
+return 0;}
+
